4482f7e7bae11c6bc964c1f169b6d08990a8cc6f
Romain GONÇALVES <romain.goncalves@gmail.com>
2018-03-21 00:42:10 +0100

    Move nav into header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,8 +2,31 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const Header = () => (
-  <div>
-  </div>
+    <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+        <div className="container">
+            <a className="navbar-brand js-scroll-trigger" href="#page-top">3rgo.me</a>
+            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i className="fa fa-bars"></i>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <a className="nav-link js-scroll-trigger" href="#about">&Agrave; propos</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link js-scroll-trigger" href="#technologies">Technologies</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link js-scroll-trigger" href="#experiences">Expériences</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link js-scroll-trigger" href="#social">Social</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 )
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,32 +3,7 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
     <div>
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-            <div className="container">
-                <a className="navbar-brand js-scroll-trigger" href="#page-top">3rgo.me</a>
-                <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                Menu
-                <i className="fa fa-bars"></i>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarResponsive">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <a className="nav-link js-scroll-trigger" href="#about">&Agrave; propos</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link js-scroll-trigger" href="#technologies">Technologies</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link js-scroll-trigger" href="#experiences">Expériences</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link js-scroll-trigger" href="#social">Social</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <header className="intro">
+        <div className="intro">
             <div className="intro-body">
                 <div className="container">
                     <div className="row">
@@ -42,7 +17,7 @@ const IndexPage = () => (
                     </div>
                 </div>
             </div>
-        </header>
+        </div>
         <section id="about" className="content-section text-center">
             <div className="container">
                 <div className="row">
