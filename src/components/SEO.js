cf6a06bf4314b215d0dc483685fb62b33527698c
Romain GONÇALVES <romain.goncalves@gmail.com>
2018-03-21 00:32:35 +0100

    ADD HTML, CSS and images from original version

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -3,25 +3,36 @@ import Helmet from "react-helmet";
 import config from "../utils/config";
 
 const SEO = () => {
-  const title = config.siteTitle;
-  const description = config.siteDescription;
-  const url = config.siteUrl;
-  const image = config.siteImage;
+    const title = config.siteTitle;
+    const description = config.siteDescription;
+    const url = config.siteUrl;
+    const image = config.siteImage;
 
-  return (
-    <Helmet>
-      <html lang="fr" />
-      {/* General tags */}
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      {/* OpenGraph tags */}
-      <meta property="og:url" content={url} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
-      <meta property="og:type" content="website" />
-    </Helmet>
-  );
+    return (
+        <Helmet>
+            <html lang="fr" />
+            {/* General tags */}
+            <title>{title}</title>
+
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+            <meta name="description" content={description} />
+
+            {/* OpenGraph tags */}
+            <meta property="og:url" content={url} />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={description} />
+            <meta property="og:image" content={image} />
+            <meta property="og:type" content="website" />
+
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+            <script src="https://use.fontawesome.com/1d6d524a55.js"></script>
+            <link href="https://fonts.googleapis.com/css?family=Biryani:400,700" rel="stylesheet" />
+
+            <body className="page-top" />
+        </Helmet>
+    );
 };
 
 export default SEO;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,225 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
+    <div>
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+            <div className="container">
+                <a className="navbar-brand js-scroll-trigger" href="#page-top">3rgo.me</a>
+                <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i className="fa fa-bars"></i>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarResponsive">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <a className="nav-link js-scroll-trigger" href="#about">&Agrave; propos</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link js-scroll-trigger" href="#technologies">Technologies</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link js-scroll-trigger" href="#experiences">Expériences</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link js-scroll-trigger" href="#social">Social</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <header className="intro">
+            <div className="intro-body">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-10 mx-auto">
+                            <h1 className="brand-heading">Romain GON&Ccedil;ALVES</h1>
+                            <p className="intro-text">Ingénieur en développement Web</p>
+                            <a href="#about" className="btn btn-circle js-scroll-trigger">
+                                <i className="fa fa-angle-double-down animated"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <section id="about" className="content-section text-center">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-10 mx-auto">
+                        <h2>&Agrave; propos de moi</h2>
+                        <p>In esse enim sint deserunt aute dolore amet eu sint nulla ad velit officia officia nostrud sunt duis sit cillum et nostrud tempor exercitation ad laborum aliqua enim sit consectetur in cillum in labore ut qui pariatur do sed laboris enim deserunt cupidatat adipisicing occaecat nostrud nostrud sed ad ex consequat consectetur eu in adipisicing ut sit cupidatat do aliqua deserunt elit laborum in excepteur dolore occaecat et consequat laboris reprehenderit officia cupidatat sint labore pariatur occaecat sint ut pariatur dolor elit enim eu aliquip ullamco culpa excepteur commodo incididunt pariatur in et exercitation do irure consectetur proident laboris pariatur consectetur incididunt incididunt deserunt minim ut sint velit culpa qui quis cillum fugiat dolor non dolor pariatur laboris do ex laboris elit consequat minim id deserunt ut dolor exercitation aute tempor anim ex magna excepteur mollit dolore quis fugiat voluptate et mollit sint veniam duis dolor dolor aliquip velit ut minim in deserunt dolor culpa incididunt cupidatat et sint dolor sed in commodo esse enim minim cupidatat aliquip elit ea deserunt exercitation dolor ut aliquip cillum nulla dolore est in pariatur fugiat non dolore ullamco ea ea duis nostrud sed et irure eiusmod velit elit eu veniam do irure nostrud non ad eiusmod ea irure voluptate laboris ut dolore ad in in occaecat voluptate est deserunt dolore reprehenderit tempor sit ut ex eu deserunt cillum consequat do dolor occaecat amet in non ut do id sunt laborum ut non exercitation in in voluptate aliquip amet velit eiusmod amet sed voluptate.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="technologies" className="content-section text-center">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-10 mx-auto">
+                        <h2>Technologies</h2>
+                        <div className="row">
+                            <div className="col-lg-4">
+                                <ul className="list-unstyled">
+                                  <li className="media">
+                                    <img className="mr-3" src="http://via.placeholder.com/150" alt="Generic placeholder image"/>
+                                    <div className="media-body">
+                                      <h5 className="mt-0 mb-1">List-based media object</h5>
+                                    </div>
+                                  </li>
+                                  <li className="media my-4">
+                                    <img className="mr-3" src="http://via.placeholder.com/150" alt="Generic placeholder image"/>
+                                    <div className="media-body">
+                                      <h5 className="mt-0 mb-1">List-based media object</h5>
+                                    </div>
+                                  </li>
+                                  <li className="media">
+                                    <img className="mr-3" src="http://via.placeholder.com/150" alt="Generic placeholder image"/>
+                                    <div className="media-body">
+                                      <h5 className="mt-0 mb-1">List-based media object</h5>
+                                    </div>
+                                  </li>
+                                </ul>
+                            </div>
+                            <div className="col-lg-4">
+                                <ul className="list-unstyled">
+                                  <li className="media">
+                                    <img className="mr-3" src="http://via.placeholder.com/150" alt="Generic placeholder image"/>
+                                    <div className="media-body">
+                                      <h5 className="mt-0 mb-1">List-based media object</h5>
+                                    </div>
+                                  </li>
+                                  <li className="media my-4">
+                                    <img className="mr-3" src="http://via.placeholder.com/150" alt="Generic placeholder image"/>
+                                    <div className="media-body">
+                                      <h5 className="mt-0 mb-1">List-based media object</h5>
+                                    </div>
+                                  </li>
+                                  <li className="media">
+                                    <img className="mr-3" src="http://via.placeholder.com/150" alt="Generic placeholder image"/>
+                                    <div className="media-body">
+                                      <h5 className="mt-0 mb-1">List-based media object</h5>
+                                    </div>
+                                  </li>
+                                </ul>
+                            </div>
+                            <div className="col-lg-4">
+                                <ul className="list-unstyled">
+                                  <li className="media">
+                                    <img className="mr-3" src="http://via.placeholder.com/150" alt="Generic placeholder image"/>
+                                    <div className="media-body">
+                                      <h5 className="mt-0 mb-1">List-based media object</h5>
+                                    </div>
+                                  </li>
+                                  <li className="media my-4">
+                                    <img className="mr-3" src="http://via.placeholder.com/150" alt="Generic placeholder image"/>
+                                    <div className="media-body">
+                                      <h5 className="mt-0 mb-1">List-based media object</h5>
+                                    </div>
+                                  </li>
+                                  <li className="media">
+                                    <img className="mr-3" src="http://via.placeholder.com/150" alt="Generic placeholder image"/>
+                                    <div className="media-body">
+                                      <h5 className="mt-0 mb-1">List-based media object</h5>
+                                    </div>
+                                  </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="experiences" className="content-section">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-10 mx-auto">
+                        <h2>Exp&eacute;riences</h2>
+                        <div className="media">
+                            <img className="align-self-start mr-3" src="http://via.placeholder.com/150" alt="Generic placeholder image"/>
+                            <div className="media-body">
+                                <h5><strong>2016 - 2018</strong>&nbsp;Lorem ipsum aliqua quis nulla.</h5>
+                                <p className="lead">Fugiat esse ut enim quis reprehenderit in anim minim labore aliqua consectetur reprehenderit.</p>
+                                <p>Irure in ex ut in pariatur amet aliqua tempor in commodo sint officia exercitation exercitation elit aliqua occaecat sit et qui in fugiat amet aliquip enim cillum non commodo cupidatat ex mollit sunt dolor sit consequat voluptate esse sint eu amet nisi velit ullamco dolor amet consequat ad non sed voluptate culpa voluptate cupidatat veniam mollit eu qui in anim officia cupidatat sed esse amet in ut dolor mollit aliqua do anim ut proident aliqua est elit reprehenderit deserunt ea do ad cupidatat magna dolore id commodo excepteur incididunt irure in duis voluptate mollit dolore enim aliquip eu occaecat eiusmod cupidatat fugiat minim nulla aute mollit dolor amet aliqua velit nulla consequat officia eu ullamco elit..</p>
+                                <h6>Technologies</h6>
+                                <dl className="row">
+                                    <dt className="col-sm-3">Lorem ipsum.</dt>
+                                    <dd className="col-sm-9">
+                                        <p>Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</p>
+                                        <p>Donec id elit non mi porta gravida at eget metus.</p>
+                                    </dd>
+                                    <dt className="col-sm-3">Lorem ipsum.</dt>
+                                    <dd className="col-sm-9">
+                                        <p>Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</p>
+                                        <p>Donec id elit non mi porta gravida at eget metus.</p>
+                                    </dd>
+                                    <dt className="col-sm-3">Lorem ipsum.</dt>
+                                    <dd className="col-sm-9">
+                                        <p>Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</p>
+                                        <p>Donec id elit non mi porta gravida at eget metus.</p>
+                                    </dd>
+                                    <dt className="col-sm-3">Lorem ipsum.</dt>
+                                    <dd className="col-sm-9">
+                                        <p>Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</p>
+                                        <p>Donec id elit non mi porta gravida at eget metus.</p>
+                                    </dd>
+                                </dl>
+                            </div>
+                        </div>
+                        <div className="media">
+                            <img className="align-self-start mr-3" src="http://via.placeholder.com/150" alt="Generic placeholder image"/>
+                            <div className="media-body">
+                                <h5><strong>2014 - 2016</strong>&nbsp;Lorem ipsum aliqua quis nulla.</h5>
+                                <p className="lead">Fugiat esse ut enim quis reprehenderit in anim minim labore aliqua consectetur reprehenderit.</p>
+                                <p>Irure in ex ut in pariatur amet aliqua tempor in commodo sint officia exercitation exercitation elit aliqua occaecat sit et qui in fugiat amet aliquip enim cillum non commodo cupidatat ex mollit sunt dolor sit consequat voluptate esse sint eu amet nisi velit ullamco dolor amet consequat ad non sed voluptate culpa voluptate cupidatat veniam mollit eu qui in anim officia cupidatat sed esse amet in ut dolor mollit aliqua do anim ut proident aliqua est elit reprehenderit deserunt ea do ad cupidatat magna dolore id commodo excepteur incididunt irure in duis voluptate mollit dolore enim aliquip eu occaecat eiusmod cupidatat fugiat minim nulla aute mollit dolor amet aliqua velit nulla consequat officia eu ullamco elit..</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="social" className="content-section text-center">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-10 mx-auto">
+                        <h2>R&eacute;seaux Sociaux</h2>
+
+                        <ul className="list-inline banner-social-buttons">
+                            <li className="list-inline-item">
+                                <a href="https://twitter.com/_3rgo" target="_blank" rel="noopener" className="btn btn-default btn-lg">
+                                    <i className="fa fa-twitter fa-fw" aria-hidden="true"></i>
+                                    <span className="network-name">Twitter</span>
+                                </a>
+                            </li>
+                            <li className="list-inline-item">
+                                <a href="https://github.com/3rgo" target="_blank" rel="noopener" className="btn btn-default btn-lg">
+                                    <i className="fa fa-github fa-fw" aria-hidden="true"></i>
+                                    <span className="network-name">Github</span>
+                                </a>
+                            </li>
+                            <li className="list-inline-item">
+                                <a href="https://www.linkedin.com/in/romain-gonçalves-83214423" target="_blank" rel="noopener" className="btn btn-default btn-lg">
+                                    <i className="fa fa-linkedin fa-fw" aria-hidden="true"></i>
+                                    <span className="network-name">LinkedIn</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <footer>
+            <div className="container text-center">
+                <p className="lead">Copyright &copy; 3rgo.me 2018</p>
+                <p className="font-weight-light">
+                    <small>
+                        Built with <a href="https://getbootstrap.com/">Bootstrap</a> and <a href="https://jquery.com/">jQuery</a>.<br/>
+                        Based on <a target="_blank" rel="noopener" href="https://github.com/BlackrockDigital/startbootstrap-grayscale">Greyscale theme by Start Bootstrap</a>
+                    </small>
+                </p>
+            </div>
+        </footer>
+    </div>
 )
 
 export default IndexPage
