1557717bb3a591ce2ad3435f6031b22e7f7819b4
Romain GONÇALVES <romain.goncalves@gmail.com>
2018-03-29 23:24:06 +0200

    Fix burger menu icon

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import faBars from '@fortawesome/fontawesome-free-solid/faBars'
 
 const Navigation = () => (
     <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
         <div className="container">
             <a className="navbar-brand js-scroll-trigger" href="#page-top">3rgo.me</a>
             <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle Navigation">
-            Menu
-            <i className="fa fa-bars"></i>
+                Menu&nbsp;
+                <FontAwesomeIcon icon={faBars} fixedWidth/>
             </button>
             <div className="collapse navbar-collapse" id="navbarResponsive">
                 <ul className="navbar-nav ml-auto">
