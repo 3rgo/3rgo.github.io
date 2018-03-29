a619331237f2a4973c8c039d844f0283d5492cf0
Romain GONÇALVES <romain.goncalves@gmail.com>
2018-03-29 23:22:01 +0200

    Add global smooth scrolling

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
+// Global smooth scrolling
+import smoothscroll from 'smoothscroll'
+
 import Navigation from '../components/Navigation'
 import SEO from '../components/SEO'
 
