98b25092982c2fa5de416d19f7bcdff2180641d4
Romain GONÇALVES <romain.goncalves@gmail.com>
2018-03-30 00:21:57 +0200

    Rework Navigation with reacstrap

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,34 +1,59 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import faBars from '@fortawesome/fontawesome-free-solid/faBars'
 
-const Navigation = () => (
-    <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-        <div className="container">
-            <a className="navbar-brand js-scroll-trigger" href="#page-top">3rgo.me</a>
-            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle Navigation">
-                Menu&nbsp;
-                <FontAwesomeIcon icon={faBars} fixedWidth/>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <a className="nav-link js-scroll-trigger" href="#about">&Agrave; propos</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link js-scroll-trigger" href="#technologies">Technologies</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link js-scroll-trigger" href="#experiences">Expériences</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link js-scroll-trigger" href="#social">Social</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-)
+export default class Navigation extends React.Component {
 
-export default Navigation;
+    constructor(props){
+        super(props);
+
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.collapseNavbar = this.collapseNavbar.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+
+    toggleNavbar(){
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    collapseNavbar(){
+        this.setState({
+            isOpen: false
+        });
+    }
+
+    render() {
+        return (
+            <Navbar light expand="lg" fixed="top" id="mainNav">
+                <NavbarBrand href="#page-top">3rgo.me</NavbarBrand>
+                <NavbarToggler onClick={this.toggleNavbar} className="navbar-toggle">
+                    Menu&nbsp;
+                    <FontAwesomeIcon icon={faBars} fixedWidth/>
+                </NavbarToggler>
+                <Collapse isOpen={this.state.isOpen} navbar id="navbarResponsive">
+                    <Nav className="navbar-nav ml-auto" navbar>
+                        <NavItem>
+                            <NavLink onClick={this.collapseNavbar} href="#about">&Agrave; propos</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink onClick={this.collapseNavbar} href="#technologies">Technologies</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink onClick={this.collapseNavbar} href="#experiences">Expériences</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink onClick={this.collapseNavbar} href="#social">Social</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        )
+    }
+}
