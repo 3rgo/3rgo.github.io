67c466c44354cc360d739d82ac3abb661d84079e
Romain GONÇALVES <romain.goncalves@gmail.com>
2018-05-02 13:01:11 +0200

    Add technologies list

--- src/utils/config.js
+++ src/utils/config.js
@@ -7,6 +7,76 @@ module.exports = {
     contactEmail: "contact@3rgo.me",
     analyticsTrackingId: "UA-110088221-3",
     content: {
-        about: "J'ai découvert l'informatique à l'âge de 12 ans à la bibliothèque municipale. À 14 ans, j'apprenais le HTML et le CSS afin de réaliser mes premières pages statiques, et à 17 ans j'y ajoutais le PHP pour dynamiser l'ensemble.<br/>Depuis je ne cesse de vouloir toujours en apprendre plus sur le monde du développement. Majoritairement auto-didacte, je prends plaisir à explorer de nouveaux langages et de nouvelles manières de solutionner les problèmes.<br/>Mes études supérieures, composées d'une classe préparatoire scientifique et d'une école d'ingénieurs m'ont apporté la rigueur et les méthodologies de travail, nécessaires afin d'être totalement autonome.<br/>Au travers mes expériences professionnelles, j'ai pu participer à des projets variés, tant par leur ampleur que par leur complexité, m'enrichissant toujours plus du contact avec d'autres développeurs et avec les équipes métier. Tout ceci en gardant en tête ma passion première : améliorer la manière dont les autres travaillent."
+        about: "J'ai découvert l'informatique à l'âge de 12 ans à la bibliothèque municipale. À 14 ans, j'apprenais le HTML et le CSS afin de réaliser mes premières pages statiques, et à 17 ans j'y ajoutais le PHP pour dynamiser l'ensemble.<br/>Depuis je ne cesse de vouloir toujours en apprendre plus sur le monde du développement. Majoritairement auto-didacte, je prends plaisir à explorer de nouveaux langages et de nouvelles manières de solutionner les problèmes.<br/>Mes études supérieures, composées d'une classe préparatoire scientifique et d'une école d'ingénieurs m'ont apporté la rigueur et les méthodologies de travail, nécessaires afin d'être totalement autonome.<br/>Au travers mes expériences professionnelles, j'ai pu participer à des projets variés, tant par leur ampleur que par leur complexité, m'enrichissant toujours plus du contact avec d'autres développeurs et avec les équipes métier. Tout ceci en gardant en tête ma passion première : améliorer la manière dont les autres travaillent.",
+        technologies: {
+            main: [
+                {name: "PHP", logo: "php.svg", url: "http://php.net"},
+                {name: "Javascript", logo: "javascript.svg", url: "https://www.javascript.com"},
+                {name: "MySQL", logo: "mysql.svg", url: "https://www.mysql.com"}
+            ],
+            full: [
+                [{name: "PHP", logo: "php.svg", url: "http://php.net", level: 5}],
+                [
+                    {name: "Laravel", logo: "laravel.svg", url: "https://laravel.com", level: 4},
+                    {name: "Symfony", logo: "symfony.svg", url: "https://symfony.com", level: 4},
+                    {name: "Slim", logo: "slim.svg", url: "https://www.slimframework.com", level: 4},
+                    {name: "Zend", logo: "zend.svg", url: "https://framework.zend.com", level: 2},
+                    {name: "Lumen", logo: "lumen.svg", url: "https://lumen.laravel.com", level: 5}
+                ],
+                [{name: "Javascript", logo: "javascript.svg", url: "https://www.javascript.com", level: 5}],
+                [
+                    {name: "jQuery", logo: "jquery.svg", url: "http://jquery.com", level: 5},
+                    {name: "React", logo: "react.svg", url: "https://reactjs.org", level: 3},
+                    {name: "AngularJS", logo: "angularjs.svg", url: "https://angularjs.org", level: 2},
+                    {name: "Node.js", logo: "nodejs.svg", url: "https://nodejs.org", level: 1},
+                    {name: "MeteorJS", logo: "meteorjs.svg", url: "https://www.meteor.com", level: 2}
+                ],
+                [
+                    {name: "MySQL / MariaDB", logo: "mysql.svg", url: "https://www.mysql.com", level: 4},
+                    {name: "PostgreSQL", logo: "pgsql.svg", url: "https://www.postgresql.org", level: 3},
+                    {name: "SQLite", logo: "sqlite.svg", url: "https://www.sqlite.org", level: 5},
+                    {name: "MongoDB", logo: "mongodb.svg", url: "https://www.mongodb.com", level: 2},
+                    {name: "Microsoft SQL Server", logo: "mssql.svg", url: "https://www.microsoft.com/sql-server/", level: 3},
+                ],
+                [
+                    {name: "HTML 5", logo: "html5.svg", url: "https://www.w3.org/html/", level: 5},
+                    {name: "CSS 3", logo: "css3.svg", url: "https://www.w3.org/Style/CSS/", level: 4},
+                    {name: "SASS", logo: "sass.svg", url: "https://sass-lang.com", level: 4},
+                    {name: "Bootstrap", logo: "bootstrap.svg", url: "https://getbootstrap.com", level: 5}
+                ],
+                [
+                    {name: "Python", logo: "python.svg", url: "https://www.python.org", level: 2},
+                    {name: "Ruby", logo: "ruby.svg", url: "https://www.ruby-lang.org", level: 2},
+                    {name: "Bash", logo: "bash.svg", url: "https://www.gnu.org/software/bash", level: 4}
+                ],
+                [
+                    {name: "Git", logo: "git.svg", url: "https://git-scm.com", level: 4},
+                    {name: "Mercurial", logo: "mercurial.svg", url: "https://www.mercurial-scm.org", level: 3}
+                ],
+                [
+                    {name: "Jenkins", logo: "jenkins.svg", url: "https://jenkins.io", level: 3},
+                    {name: "Ansible", logo: "ansible.svg", url: "https://www.ansible.com", level: 2},
+                    {name: "Webpack", logo: "webpack.svg", url: "https://webpack.js.org", level: 3}
+                ],
+                [
+                    {name: "Apache", logo: "apache.svg", url: "https://httpd.apache.org", level: 4},
+                    {name: "Nginx", logo: "nginx.svg", url: "https://nginx.org", level: 3},
+                    {name: "PHP Unit", logo: "phpunit.svg", url: "https://phpunit.de", level: 4},
+                    {name: "Jasmine", logo: "jasmine.svg", url: "https://jasmine.github.io", level: 2}
+                ],
+                [
+                    {name: "Windows", logo: "windows.svg", url: "https://www.microsoft.com/windows/", level: 5},
+                    {name: "macOS", logo: "macos.svg", url: "https://www.apple.com/macos", level: 4},
+                    {name: "GNU/Linux", logo: "linux.svg", url: "https://www.gnu.org/", level: 3}
+                ],
+                [
+                    {name: "JIRA", logo: "jira.svg", url: "https://www.atlassian.com/software/jira", level: 4},
+                    {name: "Redmine", logo: "redmine.svg", url: "https://www.redmine.org", level: 4}
+                ],
+                [
+                    {name: "Microsoft Office", logo: "office.svg", url: "https://www.office.com/", level: 4}
+                ],
+            ]
+        }
     }
 };
