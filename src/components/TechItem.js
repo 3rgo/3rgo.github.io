0f1d51d2b957277740234d4481a3b4a9acd8c3ff
Romain GONÇALVES <romain.goncalves@gmail.com>
2018-05-08 19:16:28 +0200

    Add stars display and adjust margins

--- src/components/TechItem.js
+++ src/components/TechItem.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React from 'react'
+import ReactStars from 'react-stars'
 
 const TechItem = (props) => {
     return (
-        <div className="d-flex flex-column techitem mx-3">
+        <div className="d-flex flex-column techitem mx-3 mb-2">
             <div className="px-2">
                 <img src={"http://via.placeholder.com/150?text="+props.name} alt={props.name+" logo"}/>
             </div>
@@ -11,7 +12,9 @@ const TechItem = (props) => {
                     {props.name}
                 </a>
             </div>
-            <div className="px-2">{props.level}</div>
+            <div className="mx-auto">
+                <ReactStars count={5} value={props.level} edit={false} />
+            </div>
         </div>
     )
 }
