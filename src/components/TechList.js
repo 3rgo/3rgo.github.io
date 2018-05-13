761ec9c9fcdc7f52728036ff196794e7993dacd1
Romain GONÇALVES <romain.goncalves@gmail.com>
2018-05-13 17:39:46 +0200

    Display tech list depending on screen width

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,21 +1,51 @@
-import React from 'react';
+import React from 'react'
 
 import TechItem from "../components/TechItem.js"
 
-const TechList = (props) => {
-  return (
-    <div>
-        {
-            props.items.map ((row, rowIdx) => (
-                <div key={"row-" + rowIdx} className="d-flex justify-content-around techitem-row">
-                {
-                    row.map((item, itemIdx) => (<TechItem key={"row-" + rowIdx + "-" + itemIdx} {...item} />))
-                }
-                </div>
-            ))
-        }
-    </div>
-  )
-}
+export default class TechList extends React.Component {
 
-export default TechList;
+    constructor(props){
+        super(props);
+
+        this.renderMobile = this.renderMobile.bind(this);
+        this.renderBrowser = this.renderBrowser.bind(this);
+        this.updateDimensions = this.updateDimensions.bind(this);
+    }
+
+    renderBrowser() {
+        return this.props.items.map ((row, rowIdx) => (
+            <div key={"row-" + rowIdx} className="d-flex justify-content-around techitem-row">
+            {
+                row.map((item, itemIdx) => (<TechItem key={"row-" + rowIdx + "-" + itemIdx} {...item} />))
+            }
+            </div>
+        ))
+    }
+
+    renderMobile() {
+        return "toto";
+    }
+
+    render() {
+        if (this.state.width < 992) {
+            return <div>{this.renderMobile()}</div>
+        }
+        return <div>{this.renderBrowser()}</div>
+    }
+
+    updateDimensions() {
+        this.setState({width: window.innerWidth, height: window.innerHeight});
+    }
+
+    componentWillMount() {
+        this.updateDimensions();
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", this.updateDimensions);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateDimensions);
+    }
+}
