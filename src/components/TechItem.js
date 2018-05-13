7b805440fa2f175a9b792a35e1e264d6e03148df
Romain GONÇALVES <romain.goncalves@gmail.com>
2018-05-13 21:53:00 +0200

    Improve tech list styling on mobile

--- src/components/TechItem.js
+++ src/components/TechItem.js
@@ -1,23 +1,60 @@
 import React from 'react'
+
 import ReactStars from 'react-stars'
-import { withPrefix } from 'gatsby-link'
 
-const TechItem = (props) => {
-    return (
-        <div className="d-flex flex-column techitem mx-3 mb-2">
-            <div className="px-2 d-flex img-wrapper justify-content-center align-content-center">
-                <img src={'static/logos/'+props.logo} alt={props.name+" logo"} title={props.name+" logo"}/>
+export default class TechItem extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            screenType: this.getScreenType(props.screenWidth)
+        }
+    }
+
+    getScreenType(width){
+        if(width < 768){
+            return 'xs';
+        } else if(width >= 768 && width < 992){
+            return 'sm';
+        } else {
+            return 'lg';
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const screenType = this.getScreenType(nextProps.screenWidth);
+        if(this.state.screenType != screenType){
+            this.setState({
+                screenType: screenType
+            });
+        }
+    }
+
+    render() {
+        let cls = "d-flex flex-column techitem";
+        if(this.state.screenType == "xs"){
+            cls += " mx-1 mb-0 xsmall";
+        } else if (this.state.screenType =="sm"){
+            cls += " mx-2 mb-0 small";
+        } else {
+            cls += " mx-3 mb-2";
+        }
+
+        return (
+            <div className={cls}>
+                <div className="px-2 d-flex img-wrapper justify-content-center align-content-center">
+                    <img src={'static/logos/'+this.props.logo} alt={this.props.name+" logo"} title={this.props.name+" logo"}/>
+                </div>
+                <div className="px-0 text-center">
+                    <a href={this.props.url} target="_blank" rel="noopener">
+                        {this.props.name}
+                    </a>
+                </div>
+                <div className="mx-auto">
+                    <ReactStars count={5} value={this.props.level} size={12} edit={false} />
+                </div>
             </div>
-            <div className="px-2">
-                <a href={props.url} target="_blank" rel="noopener">
-                    {props.name}
-                </a>
-            </div>
-            <div className="mx-auto">
-                <ReactStars count={5} value={props.level} edit={false} />
-            </div>
-        </div>
-    )
+        )
+    }
 }
-
-export default TechItem;
--- src/components/TechList.js
+++ src/components/TechList.js
@@ -6,31 +6,21 @@ export default class TechList extends React.Component {
 
     constructor(props){
         super(props);
-
-        this.renderMobile = this.renderMobile.bind(this);
-        this.renderBrowser = this.renderBrowser.bind(this);
         this.updateDimensions = this.updateDimensions.bind(this);
     }
 
-    renderBrowser() {
-        return this.props.items.map ((row, rowIdx) => (
-            <div key={"row-" + rowIdx} className="d-flex justify-content-around techitem-row">
-            {
-                row.map((item, itemIdx) => (<TechItem key={"row-" + rowIdx + "-" + itemIdx} {...item} />))
-            }
-            </div>
-        ))
-    }
-
-    renderMobile() {
-        return "toto";
-    }
-
     render() {
-        if (this.state.width < 992) {
-            return <div>{this.renderMobile()}</div>
-        }
-        return <div>{this.renderBrowser()}</div>
+        return (<div>
+            {
+                this.props.items.map ((row, rowIdx) => (
+                    <div key={"row-" + rowIdx} className="d-flex justify-content-around techitem-row">
+                    {
+                        row.map((item, itemIdx) => (<TechItem key={"row-" + rowIdx + "-" + itemIdx} {...item} screenWidth={this.state.width} />))
+                    }
+                    </div>
+                ))
+            }
+        </div>)
     }
 
     updateDimensions() {
